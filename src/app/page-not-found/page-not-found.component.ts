52e9e894de5ef6301a7e948e0db6b0169687e8c1
jcmann <jen.c.mann@gmail.com>
2021-05-04 14:45:14 -0500

    Added 404 page

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-page-not-found',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageNotFoundComponent implements OnInit {
 
-  constructor() { }
+    // use router instead of location to reset history
+  constructor(private router : Router) { }
 
   ngOnInit(): void {
+  }
+
+  startOver() {
+      this.router.navigateByUrl("/");
   }
 
 }
